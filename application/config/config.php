1ac0d9ac89750b6825d6cc95c0c2149b339f60bb
nur.hossen <nur.hossen@datasoft-bd.com>
2018-09-08 10:46:37 +0600

    modify initial config files and update composer.json

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+
+//this base url for local development ---- like base path  http://localhost/sdf_lms
+$config['base_url'] = 'http://' . $_SERVER['HTTP_HOST'] . '/' . explode('/', $_SERVER['REQUEST_URI'])[1];
+
+
+//this base url for live development in root folder ---- like base path http://27.147.155.45/
+//$config['base_url'] = 'http://' . $_SERVER['HTTP_HOST'] . '/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +41,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -52,7 +58,7 @@ $config['index_page'] = 'index.php';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'REQUEST_URI';
+$config['uri_protocol'] = 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
@@ -76,7 +82,13 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+
+
+$config['username_min_length'] = 3;
+$config['username_max_length'] = 20;
+$config['password_min_length'] = 2;
+$config['password_max_length'] = 12;
+$config['language'] = 'english';
 
 /*
 |--------------------------------------------------------------------------
@@ -400,11 +412,11 @@ $config['sess_regenerate_destroy'] = FALSE;
 |       'cookie_httponly') will also affect sessions.
 |
 */
-$config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
-$config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
-$config['cookie_httponly'] 	= FALSE;
+$config['cookie_prefix'] = '';
+$config['cookie_domain'] = '';
+$config['cookie_path'] = '/';
+$config['cookie_secure'] = FALSE;
+$config['cookie_httponly'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
